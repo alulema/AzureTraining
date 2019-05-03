95e2c633b093c27053c11044ab851bcc21f72f7d
Alexis Alulema <alexis.alulema@gmail.com>
2019-05-03 16:55:29 -0500

    Adding AppInisghts to PostTransaction

--- BeginningAzureServerlessArchitecture/BeginningAzureServerlessArchitecture/PostTransactions.cs
+++ BeginningAzureServerlessArchitecture/BeginningAzureServerlessArchitecture/PostTransactions.cs
@@ -1,15 +1,26 @@
+using Alulema.Function.Models;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using System.Net.Http;
+using System;
 using System.Net;
-using Alulema.Function.Models;
+using System.Net.Http;
 
 namespace Alulema.Function
 {
     public static class PostTransactions
     {
+        private static string key = TelemetryConfiguration.Active.InstrumentationKey =
+            Environment.GetEnvironmentVariable("APPINSIGHTS_INSTRUMENTATIONKEY", EnvironmentVariableTarget.Process);
+
+        private static TelemetryClient telemetry = new TelemetryClient
+        {
+            InstrumentationKey = key
+        };
+
         [FunctionName("PostTransactions")]
         public static HttpResponseMessage Run(
             [HttpTrigger(
@@ -25,7 +36,19 @@ namespace Alulema.Function
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             var message = req.Content.ReadAsStringAsync().Result;
-            transaction = JsonConvert.DeserializeObject<Transaction>(message);
+
+            try
+            {
+                transaction = JsonConvert.DeserializeObject<Transaction>(message);
+            }
+            catch (Exception e)
+            {
+                log.LogError($"Invalid payload received, input received was: {message}");
+                telemetry.TrackEvent("Bad request received");
+                telemetry.TrackException(e);
+                transaction = null;
+                return req.CreateErrorResponse(HttpStatusCode.BadRequest, "The request did not match the required schema");
+            }
 
             return req.CreateResponse(HttpStatusCode.OK, $"You made a transaction of {transaction.Amount}");
         }
